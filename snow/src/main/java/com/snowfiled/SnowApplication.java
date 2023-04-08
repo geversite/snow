f43ec693be92819e7ac409fcce5930505f8ef0f7
Patrick <luwenhao369@icloud.com>
2023-04-08 22:36:56 +0800

    将port 改为80 配置了log框架

--- snow/src/main/java/com/snowfiled/SnowApplication.java
+++ snow/src/main/java/com/snowfiled/SnowApplication.java
@@ -8,7 +8,6 @@ public class SnowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SnowApplication.class, args);
-        System.out.println("zxbzxb!!");
     }
 
 }
--- snow/src/main/java/com/snowfiled/mapper/PayMapper.java
+++ snow/src/main/java/com/snowfiled/mapper/PayMapper.java
@@ -1,11 +1,13 @@
 package com.snowfiled.mapper;
 
 import com.snowfiled.bean.PeopleTimeData;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.Date;
 import java.util.List;
 
+@Mapper
 public interface PayMapper {
 
     // TODO: 2023/4/3 不知道对不对
